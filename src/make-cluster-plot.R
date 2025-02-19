19b113febb0e16d9f5d1e3f4f2e10371f738bb7b
Daniel J. McDonald <dajmcdon@gmail.com>
2025-02-18 16:58:17 -0800

    lots of styling

--- src/make-cluster-plot.R
+++ src/make-cluster-plot.R
@@ -21,26 +21,28 @@ m2 <- group_means_upper_tri(sqrt(mat2), main_clusts)
 m2 <- reshape2::melt(t(m2[nrow(m2):1, ]), na.rm = TRUE)
 sply <- length(ss) - cumsum(rle(main_clusts)$lengths)[1:3] + .5
 splx <- cumsum(rle(main_clusts)$lengths)[1:3] + .5
-ggplot(m2, aes(Var1, Var2, fill = value)) + 
-  geom_raster() + 
+ggplot(m2, aes(Var1, Var2, fill = value)) +
+  geom_raster() +
   scale_fill_viridis_c(direction = -1) +
   #theme_void() +
   coord_equal() +
   xlab("") + ylab("") +
-  geom_hline(yintercept = sply, color = "#ffffff", size = .2) +
-  geom_vline(xintercept = splx, color = "#ffffff", size = .2) +
+  geom_hline(yintercept = sply, color = "#f3f3f3", size = .2) +
+  geom_vline(xintercept = splx, color = "#f3f3f3", size = .2) +
   theme(legend.position = "none", plot.margin = margin(),
         axis.ticks.x = element_blank(),
         axis.text.x = element_blank(),
-        panel.background = element_blank(),
-        panel.grid = element_blank())
+        panel.grid = element_blank(),
+        plot.background = element_blank())
+
+ggsave("gfx/other-rem-alt1-1.png", dpi = 300, bg = "transparent", width=8, height=5)
 
 
-
-if(colorthem) sdends = sdends %>% 
+library(dendextend)
+if(colorthem) sdends = sdends %>%
   set('labels_col', value=fivecolors[c(4,3,1,2)], k=nclusts) %>%
   set('branches_lty', 1) %>%
   set('branches_k_color', value=fivecolors[c(4,3,1,2)], k=nclusts)
-library(dendextend)
-ggplot(as.ggdend(sdends), labels = FALSE)
-
+ggplot(as.ggdend(sdends), labels = FALSE) +
+  theme(panel.grid = element_blank(), plot.background = element_blank())
+ggsave("gfx/other-rem-alt2-1.png", dpi = 300, bg = "transparent", width=5, height=1)
